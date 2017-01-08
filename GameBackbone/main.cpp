f0903823435b43f2b59998c0398002cfadd665de
Ryan Lavin <lavinrp@gmail.com>
2017-01-08 15:02:34 -0500

    test texture and sprite. Added uml for base classes

--- GameBackbone/main.cpp
+++ GameBackbone/main.cpp
@@ -1,19 +1,35 @@
 #include <SFML/Graphics.hpp>
 
 int main() {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::RenderWindow window(sf::VideoMode(700, 700), "SpriteTests");
 	sf::CircleShape shape(100.f);
 	shape.setFillColor(sf::Color::Green);
 
 	while (window.isOpen()) {
+		//close window
 		sf::Event event;
 		while (window.pollEvent(event)) {
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
 
+		//draw loop
 		window.clear();
-		window.draw(shape);
+		//draw logic here
+
+		sf::Texture testSpriteTexture;
+		testSpriteTexture.loadFromFile("..\\Textures\\testSprite.png");
+
+		sf::Sprite s(testSpriteTexture);
+
+		window.draw(s);
+		
+
+
+		//window.draw(shape);
+
+
+		//end draw logic
 		window.display();
 	}
 
