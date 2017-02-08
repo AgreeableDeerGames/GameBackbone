2dc5a30b5dca2c1af53971c69404f1d2156f20db
Ryan Lavin <lavinrp@gmail.com>
2017-02-07 23:50:12 -0500

    Implementation of PathFinder's constructors.

--- GameBackbone/PathFinder.cpp
+++ GameBackbone/PathFinder.cpp
@@ -1,4 +1,12 @@
 #include "PathFinder.h"
 
-Pathfinder::Pathfinder() {
+
+///<summary>Creates a PathFinder with a null navigation grid</summary>
+Pathfinder::Pathfinder() : Pathfinder(nullptr){}
+
+///<summary>creates a PathFinder with an assigned navigation grid</summary>
+///<param name = "navigationGrid"> Three dimensional grid to be used when path-finding</param>
+Pathfinder::Pathfinder(NavigationGrid * navigationGrid) {
+	this->navigationGrid = navigationGrid;
 }
+
