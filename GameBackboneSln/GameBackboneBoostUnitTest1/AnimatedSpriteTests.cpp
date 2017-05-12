2c7271eccae5c19823a9ab2e0c7ea48f4544482e
Ryan Lavin <lavinrp@gmail.com>
2017-05-11 23:36:56 -0400

    i73 - Added test for running an animation with only one frame.Formatted comments to standard.

--- GameBackboneSln/GameBackboneBoostUnitTest1/AnimatedSpriteTests.cpp
+++ GameBackboneSln/GameBackboneBoostUnitTest1/AnimatedSpriteTests.cpp
@@ -46,15 +46,15 @@ struct ReusableObjects
 };
 
 
-//keep at start of file
+// keep at start of file
 
-//Contains all of the tests for AnimatedSprite
+// Contains all of the tests for AnimatedSprite
 BOOST_AUTO_TEST_SUITE(AnimatedSpriteTests)
 
-//Contains all of the tests for AnimatedSprite constructors
+// Contains all of the tests for AnimatedSprite constructors
 BOOST_AUTO_TEST_SUITE(AnimatedSprite_ctrs)
 
-//Tests the AnimatedSprite default constructor
+// Tests the AnimatedSprite default constructor
 BOOST_FIXTURE_TEST_CASE(AnimatedSprite_default_ctr, ReusableObjects) {
 	AnimatedSprite* animSprite = new AnimatedSprite();
 
@@ -67,7 +67,7 @@ BOOST_FIXTURE_TEST_CASE(AnimatedSprite_default_ctr, ReusableObjects) {
 	delete animSprite;
 }
 
-//Tests the AnimatedSprite constructor that takes in a texture
+// Tests the AnimatedSprite constructor that takes in a texture
 BOOST_FIXTURE_TEST_CASE(AnimatedSprite_Texture_ctr, ReusableObjects) {
 	AnimatedSprite* animSprite = new AnimatedSprite(*aSpriteTexture);
 
@@ -83,7 +83,7 @@ BOOST_FIXTURE_TEST_CASE(AnimatedSprite_Texture_ctr, ReusableObjects) {
 	delete animSprite;
 }
 
-//Tests the AnimatedSprite constructor that takes a texture and an AnimationSet
+// Tests the AnimatedSprite constructor that takes a texture and an AnimationSet
 BOOST_FIXTURE_TEST_CASE(AnimatedSprite_Texture_and_Animations, ReusableObjects) {
 
 	//ensure that everything is set to null or 0
@@ -103,12 +103,12 @@ BOOST_FIXTURE_TEST_CASE(AnimatedSprite_Texture_and_Animations, ReusableObjects) 
 	BOOST_CHECK(animSpriteWithAnim->getCurrentFrame() == 1);
 }
 
-BOOST_AUTO_TEST_SUITE_END() //end AnimatedSprite_ctrs
+BOOST_AUTO_TEST_SUITE_END() // end AnimatedSprite_ctrs
 
-//Contains all of the tests relating to AnimatedSprite's animations
+// Contains all of the tests relating to AnimatedSprite's animations
 BOOST_AUTO_TEST_SUITE(AnimatedSprite_Animations)
 
-//test starting an animation with AnimatedSprite::RunAnimation
+// test starting an animation with AnimatedSprite::RunAnimation
 BOOST_FIXTURE_TEST_CASE(AnimatedSprite_Start_Animation, ReusableObjects) {
 	animSpriteWithAnim->runAnimation(0);
 
@@ -122,7 +122,7 @@ BOOST_FIXTURE_TEST_CASE(AnimatedSprite_Start_Animation, ReusableObjects) {
 	BOOST_CHECK(animSpriteWithAnim->getCurrentFrame() == 0);
 }
 
-//Check to see if AnimatedSprite can successfully change frames when animating
+// Check to see if AnimatedSprite can successfully change frames when animating
 BOOST_FIXTURE_TEST_CASE(AnimatedSprite_Animation_Next_Frame, ReusableObjects) {
 	animSpriteWithAnim->runAnimation(0);
 	animSpriteWithAnim->setAnimationDelay(2);
@@ -134,7 +134,7 @@ BOOST_FIXTURE_TEST_CASE(AnimatedSprite_Animation_Next_Frame, ReusableObjects) {
 	BOOST_CHECK(animSpriteWithAnim->getCurrentFrame() == 1);
 }
 
-//Check that AnimatedSprite does not change frames before the minimum time has elapsed
+// Check that AnimatedSprite does not change frames before the minimum time has elapsed
 BOOST_FIXTURE_TEST_CASE(AnimatedSprite_Animation_Next_Frame_Not_Enough_Time, ReusableObjects) {
 	animSpriteWithAnim->runAnimation(0);
 	animSpriteWithAnim->setAnimationDelay(2);
@@ -146,7 +146,7 @@ BOOST_FIXTURE_TEST_CASE(AnimatedSprite_Animation_Next_Frame_Not_Enough_Time, Reu
 	BOOST_CHECK(animSpriteWithAnim->getCurrentFrame() == 0);
 }
 
-//Tests that AnimatedSprite loops its animations
+// Tests that AnimatedSprite loops its animations
 BOOST_FIXTURE_TEST_CASE(AnimatedSprite_Animation_Loop_After_End, ReusableObjects) {
 	animSpriteWithAnim->runAnimation(0);
 	animSpriteWithAnim->setAnimationDelay(1);
@@ -163,7 +163,7 @@ BOOST_FIXTURE_TEST_CASE(AnimatedSprite_Animation_Loop_After_End, ReusableObjects
 	BOOST_CHECK(animSpriteWithAnim->getCurrentFrame() == 0);
 }
 
-//Test running an empty animation
+// Test running an empty animation
 BOOST_FIXTURE_TEST_CASE(AnimatedSprite_Start_Empty_Animation, ReusableObjects) {
 	std::vector<unsigned int> singleFrameAnim = {0};
 	std::vector<unsigned int> emptyAnim;
@@ -175,7 +175,29 @@ BOOST_FIXTURE_TEST_CASE(AnimatedSprite_Start_Empty_Animation, ReusableObjects) {
 	BOOST_CHECK_THROW(emptyAnimSprite.runAnimation(1), AnimatedSprite_EmptyAnimation);
 }
 
-//Ensures that animations set with setAnimations can be successfully run
+// Test running an animation with one frame
+BOOST_FIXTURE_TEST_CASE(AnimatedSprite_Run_Animation_Single_Frame, ReusableObjects) {
+	std::vector<unsigned int> singleFrameAnim = { 0 };
+	std::vector<std::vector<unsigned int>> animVector = { singleFrameAnim };
+	AnimationSet animSetSingleFrameAnim(animVector, 1, 1, 1, 1);
+	AnimatedSprite singleFrameAnimSprite(*aSpriteTexture, &animSetSingleFrameAnim);
+
+	singleFrameAnimSprite.setAnimationDelay(1);
+
+	//update the sprite one frame farther than the length of the animation
+	const int LOOP_LENGTH = 100;
+	for (int i = 0; i < LOOP_LENGTH; ++i)
+	{
+		//fake sleep for 1ms longer than the min delay
+		sf::Time timeAfterDelay = sf::milliseconds(2 * i);
+		singleFrameAnimSprite.update(timeAfterDelay);
+		BOOST_CHECK(singleFrameAnimSprite.getCurrentFrame() == 0);
+	}
+
+	BOOST_CHECK(singleFrameAnimSprite.getCurrentFrame() == 0);
+}
+
+// Ensures that animations set with setAnimations can be successfully run
 BOOST_FIXTURE_TEST_CASE(AnimatedSprite_setAnimations, ReusableObjects) {
 	AnimatedSprite* animSprite = new AnimatedSprite(*aSpriteTexture);
 
@@ -193,13 +215,13 @@ BOOST_FIXTURE_TEST_CASE(AnimatedSprite_setAnimations, ReusableObjects) {
 	delete animSprite;
 }
 
-//ensure that the minimum animation delay is stored correctly
+// ensure that the minimum animation delay is stored correctly
 BOOST_FIXTURE_TEST_CASE(AnimatedSprite_setAnimationDelay, ReusableObjects) {
 	animSpriteWithAnim->setAnimationDelay(1);
 	BOOST_CHECK_EQUAL(animSpriteWithAnim->getAnimationDelay(), 1);
 }
 
-//ensure that the current frame of an animation can be set and retrieved correctly
+// ensure that the current frame of an animation can be set and retrieved correctly
 BOOST_FIXTURE_TEST_CASE(AnimatedSprite_setCurrentFrame, ReusableObjects) {
 	animSpriteWithAnim->runAnimation(0);
 	animSpriteWithAnim->setCurrentFrame(3);
@@ -207,7 +229,7 @@ BOOST_FIXTURE_TEST_CASE(AnimatedSprite_setCurrentFrame, ReusableObjects) {
 }
 
 
-BOOST_AUTO_TEST_SUITE_END() //end AnimatedSprite_Animations
+BOOST_AUTO_TEST_SUITE_END() // end AnimatedSprite_Animations
 
-//Keep at end of file
+// Keep at end of file
 BOOST_AUTO_TEST_SUITE_END()
