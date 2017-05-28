4143a5ff66daf7182b1883e2fa83890715aa8b07
Ryan Lavin <lavinrp@gmail.com>
2017-05-27 22:02:11 -0400

    i51 - TGUI experimentation.

---
+++ GameBackboneSln/GameBackboneExe/SampleGuiInterface.cpp
@@ -0,0 +1,23 @@
+#include "stdafx.h"
+#include "SampleGuiInterface.h"
+
+
+SampleGuiInterface::SampleGuiInterface(sf::RenderWindow & attachedWindow) {
+	
+	auto theme = tgui::Theme::create("TGUI_Widgets/Black.txt");
+	gui = new tgui::Gui(attachedWindow);
+
+	//get window width and height
+	auto windowWidth = tgui::bindWidth(*gui);
+	auto windowHeight = tgui::bindHeight(*gui);
+
+	tgui::EditBox::Ptr editBoxSearch = theme->load("EditBox");
+	editBoxSearch->setSize(windowWidth * 2 / 3.0f, windowHeight / 10.0f);
+	editBoxSearch->setDefaultText("test text");
+	editBoxSearch->setPosition(0, windowHeight);
+	gui->add(editBoxSearch);
+}
+
+SampleGuiInterface::~SampleGuiInterface() {
+	delete gui;
+}
--- GameBackboneSln/GameBackboneExe/main.cpp
+++ GameBackboneSln/GameBackboneExe/main.cpp
@@ -6,6 +6,9 @@
 #include "Updatable.h"
 #include "CompoundSprite.h"
 #include "NavigationDemoRegion.h"
+#include "SampleGuiInterface.h"
+
+#include <TGUI/TGUI.hpp>
 
 #include <SFML/Graphics.hpp>
 #include <SFML/System/Clock.hpp>
@@ -16,9 +19,32 @@
 using namespace GB;
 
 int main() {
+	//Init Window
 	const int WINDOW_WIDTH = 700;
 	const int WINDOW_HEIGHT = 700;
 	sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "SpriteTests");
+	try
+	{
+		//init GUI
+		auto theme = tgui::Theme::create("TGUI_Widgets/Black.txt");
+		tgui::Gui* gui = new tgui::Gui(window);
+
+		//get window width and height
+		auto windowWidth = tgui::bindWidth(*gui);
+		auto windowHeight = tgui::bindHeight(*gui);
+
+		tgui::EditBox::Ptr editBoxSearch = theme->load("EditBox");
+		editBoxSearch->setSize(windowWidth * 2 / 3.0f, windowHeight / 10.0f);
+		editBoxSearch->setDefaultText("test text");
+		editBoxSearch->setPosition(0, windowHeight);
+		gui->add(editBoxSearch);
+
+	}
+	catch (const tgui::Exception e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
 	sf::CircleShape shape(10.0f);
 	shape.setFillColor(sf::Color::Green);
 
