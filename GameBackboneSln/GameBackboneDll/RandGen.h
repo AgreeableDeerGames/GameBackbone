0cabb849ddc4d1464533310ee5f597a0c0d8eb39
Ryan Lavin <lavinrp@gmail.com>
2017-06-18 14:36:02 -0400

    i81 - work on exporting std classes

--- GameBackboneSln/GameBackboneDll/RandGen.h
+++ GameBackboneSln/GameBackboneDll/RandGen.h
@@ -3,12 +3,32 @@
 #include "DllUtil.h"
 
 
-#include<random>
-#include<string>
+#include <random>
+#include <string>
 
-//template class libGameBackbone std::mersenne_twister_engine<unsigned int,32,624,397,31,2567483615,11,4294967295,7,2636928640,15,4022730752,18,1812433253>;
-//template class __declspec(dllexport) std::string;
+template class libGameBackbone std::allocator<char>;
+template struct libGameBackbone std::char_traits<char>;
+
+template struct libGameBackbone std::_Simple_types<char>;
+
+template union libGameBackbone std::_String_val<std::_Simple_types<char>>::_Bxty;
+
+template class libGameBackbone std::_String_val<std::_Simple_types<char>>;
+
+template class libGameBackbone std::_Compressed_pair<std::_Wrap_alloc<std::allocator<char>>, std::_String_val<std::_Simple_types<char>>, true>;
+template class libGameBackbone std::basic_string<char, std::char_traits<char>, std::allocator<char>>;
+
 template class libGameBackbone std::uniform_real_distribution<double>;
+template class libGameBackbone std::mersenne_twister_engine<unsigned int,32,624,397,31,2567483615,11,4294967295,7,2636928640,15,4022730752,18,1812433253>;
+
+
+//template class libGameBackbone std::_Compressed_pair<std::_Wrap_alloc<std::allocator<char>>, std::_String_val<std::_Simple_types<char>>, true>;
+
+
+//template class __declspec(dllexport) std::allocator<int>;
+//template class __declspec(dllexport) std::vector<int>;
+//template class __declspec(dllexport) std::string;
+
 
 namespace GB {
 
