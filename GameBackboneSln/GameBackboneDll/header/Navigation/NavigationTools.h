1ae9dbf74327ff0b86e99ed17c736679f1f7754d
Ryan Lavin <lavinrp@gmail.com>
2017-07-02 16:53:12 -0400

    i91 - declaration of some of the movement helper functions that GB will be implementing.

--- GameBackboneSln/GameBackboneDll/header/Navigation/NavigationTools.h
+++ GameBackboneSln/GameBackboneDll/header/Navigation/NavigationTools.h
@@ -1,15 +1,24 @@
 #pragma once
 
-#include <Util\DllUtil.h>
-#include <Util\Array2D.h>
 #include <Navigation\NavigationGridData.h>
+#include <Util\Array2D.h>
+#include <Util\DllUtil.h>
+#include <Util\Point.h>
 
+#include <SFML\Graphics.hpp>
+
+#include <list>
+#include <vector>
 
 
 namespace GB {
 	libGameBackbone typedef Array2D<NavigationGridData*> NavigationGrid;
 
-
 	libGameBackbone extern void initAllNavigationGridValues(NavigationGrid & navGrid, NavigationGridData & gridData);
 	libGameBackbone extern void freeAllNavigationGridData(NavigationGrid & navGrid);
+
+	libGameBackbone extern void moveSpriteToPoint(sf::Sprite& sprite, const sf::Vector2f& destination, const float pixelsPerMs);
+	libGameBackbone extern void moveAllSpritesToPoint(const std::vector<sf::Sprite*>& sprites, const std::vector<const sf::Vector2f>& destinations, const std::vector<const float>& pixelsPerMs);
+	libGameBackbone extern void moveSpriteStepTowardsPoint(sf::Sprite& sprite, const sf::Vector2f& destination, const float maxStepLength);
+
 }
