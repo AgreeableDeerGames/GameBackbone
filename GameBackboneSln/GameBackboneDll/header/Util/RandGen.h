78a1f55a1236101601008e1c1533dca14eaeff4a
Michael Gyurgyak <gyurgyma@mail.uc.edu>
2017-07-20 21:59:19 -0400

    i96-DocumentRandGen: I've added a comment. How's it sound?

--- GameBackboneSln/GameBackboneDll/header/Util/RandGen.h
+++ GameBackboneSln/GameBackboneDll/header/Util/RandGen.h
@@ -7,7 +7,11 @@
 
 
 namespace GB {
-
+	
+	/// <summary>
+	/// This class is our answer to the trash that is rand(). It is a shallow wrapper for std::random.
+	/// To use this class, make an instance of RandGen with a given seed. Call uniDist anytime you need a random number.
+	/// </summary>
 	class libGameBackbone RandGen {
 	public:
 		RandGen();
