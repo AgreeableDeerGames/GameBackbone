0abba9a6a42302bcefe13c8a5155d4b8da1d5dba
Ryan Lavin <lavinrp@gmail.com>
2017-07-29 22:55:55 -0400

    i91 - formatting

--- GameBackboneSln/GameBackboneDll/header/Navigation/NavigationTools.h
+++ GameBackboneSln/GameBackboneDll/header/Navigation/NavigationTools.h
@@ -15,16 +15,29 @@
 namespace GB {
 	libGameBackbone typedef Array2D<NavigationGridData*> NavigationGrid;
 
+	//---------------------------------------------------------------------------------------------------------------------
+
+
 	//NavigationGrid memory
 	libGameBackbone extern void initAllNavigationGridValues(NavigationGrid & navGrid, NavigationGridData & gridData);
 	libGameBackbone extern void freeAllNavigationGridData(NavigationGrid & navGrid);
 
+	//---------------------------------------------------------------------------------------------------------------------
+
 	//sprite movement to point
-	libGameBackbone extern void moveSpriteStepTowardsPoint(sf::Sprite& sprite, const sf::Vector2f& destination, const float maxStepLength, const bool orientSpriteToDestination = true);
+	libGameBackbone extern void moveSpriteStepTowardsPoint(sf::Sprite& sprite,
+														   const sf::Vector2f& destination,
+														   const float maxStepLength,
+														   const bool orientSpriteToDestination = true);
+
+
 	libGameBackbone extern void bulkMoveSpriteStepTowardsPoint(const std::vector<sf::Sprite*>& sprites, 
 																const std::vector<sf::Vector2f>& destinations,
 																const std::vector<float>& maxStepLength,
 																const bool orientSpritesToDestination = true);
+
+	//---------------------------------------------------------------------------------------------------------------------
+
 
 	//sprite movement along path
 	libGameBackbone extern void moveSpriteAlongPath(sf::Sprite& sprite, 
@@ -39,4 +52,8 @@ namespace GB {
 														 const sf::Uint64 msPassed,
 														 const std::vector<float>& distPerMs,
 													     const bool orientSpritesToDestination = true);
+
+	//---------------------------------------------------------------------------------------------------------------------
+
+
 }
