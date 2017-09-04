c0e861793e0a71dc4b256df8733a559bc634a8ab
Ryan Lavin <lavinrp@gmail.com>
2017-09-03 20:54:04 -0400

    i101 - Test all ctrs: formatting, spelling, and random ";"s

--- GameBackboneSln/GameBackboneBoostUnitTest1/source/ClusterGreenhouseTests.cpp
+++ GameBackboneSln/GameBackboneBoostUnitTest1/source/ClusterGreenhouseTests.cpp
@@ -8,24 +8,24 @@
 using namespace GB;
 
 
-//Suite containing all cluster tests.
+// Suite containing all cluster tests.
 BOOST_AUTO_TEST_SUITE(ClusterGreenhouse_Tests);
 
-//Suite containing all cluster constructor tests.
+// Suite containing all cluster constructor tests.
 BOOST_AUTO_TEST_SUITE(ClusterGreenhouse_Ctrs);
 
-//Testing the ctr with dimensions 0 by 0 to make sure there are no intrinsic leaks.
+// Testing the ctr with dimensions 0 by 0 to make sure there are no intrinsic leaks.
 BOOST_AUTO_TEST_CASE(ClusterGreenhouse_ZeroDimensions_Ctr) {
 
-	//Demensions of 0 by 0.
+	// Dimensions of 0 by 0.
 	Point2D<int> testPoint{ 0,0 };
 
-	//ClusterGreenhouse created with dimensions 0,0.
+	// ClusterGreenhouse created with dimensions 0,0.
 	ClusterGreenhouse* testCluster = new ClusterGreenhouse(testPoint);
 
 	delete testCluster;
 }
 
-BOOST_AUTO_TEST_SUITE_END(); // End ClusterGreenhouse_Ctrs
+BOOST_AUTO_TEST_SUITE_END() // End ClusterGreenhouse_Ctrs
 
 BOOST_AUTO_TEST_SUITE_END() //End ClusterGreenhouse_Tests
--- GameBackboneSln/GameBackboneBoostUnitTest1/source/ClusterTests.cpp
+++ GameBackboneSln/GameBackboneBoostUnitTest1/source/ClusterTests.cpp
@@ -8,16 +8,16 @@
 using namespace GB;
 
 
-//Suite containing all cluster tests.
+// Suite containing all cluster tests.
 BOOST_AUTO_TEST_SUITE(Cluster_Tests);
 
-//Suite containing all cluster constructor tests.
+// Suite containing all cluster constructor tests.
 BOOST_AUTO_TEST_SUITE(Cluster_Ctrs);
 
-//Testing the ctr at initial point (0,0) to make sure there are no intrinsic leaks and that points are correctly set.
+// Testing the ctr at initial point (0,0) to make sure there are no intrinsic leaks and that points are correctly set.
 BOOST_AUTO_TEST_CASE(Cluster_ZeroPoint_Ctr) {
 
-	//Point at (0,0).
+	// Point at (0,0).
 	Point2D<int> testPoint{0,0};
 
 	// Create border points to compare against.
@@ -27,16 +27,16 @@ BOOST_AUTO_TEST_CASE(Cluster_ZeroPoint_Ctr) {
 	testBorderPoints->insert(Point2D<int>{-1, 0});
 	testBorderPoints->insert(Point2D<int>{0, -1});
 
-	//Cluster created at (0,0).
+	// Cluster created at (0,0).
 	Cluster* testCluster = new Cluster(testPoint);
 
-	//Ensure the border points are what they should be.
+	// Ensure the border points are what they should be.
 	BOOST_CHECK( *testCluster->getBorderPointSet() == *testBorderPoints );
 
 	delete testBorderPoints;
 	delete testCluster;
 }
 
-BOOST_AUTO_TEST_SUITE_END(); // End Cluster_Ctrs
+BOOST_AUTO_TEST_SUITE_END() // End Cluster_Ctrs
 
-BOOST_AUTO_TEST_SUITE_END() //End Cluster_Tests
+BOOST_AUTO_TEST_SUITE_END() // End Cluster_Tests
