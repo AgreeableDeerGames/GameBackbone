35555bad58ca0cb34244e6f0aaed02e85eada074
Ryan Lavin <lavinrp@gmail.com>
2018-06-15 23:53:26 -0400

    i130 - linux_cmake: FindGB Work
    Continued work with FindGB.cmake
    Addition of -d postfix for libgamebackbone debug builds.
    
    FindGB.cmake should now find the difference between debug GB and release GB.

--- GameBackboneSln/FindGB.cmake
+++ GameBackboneSln/FindGB.cmake
@@ -1,6 +1,7 @@
 
 set(FIND_GB_PATHS
-	${GB_ROOT}
+    ${GB_ROOT}
+    $ENV{GB_ROOT}
     ~/Library/Frameworks
     /Library/Frameworks
     /usr/local
@@ -11,34 +12,55 @@ set(FIND_GB_PATHS
     /opt)
 
 
-find_library(GB_LIBRARY_STATIC_RELEASE
+find_library(GB_LIBRARY_DYNAMIC_RELEASE
              NAMES GameBackbone
              PATH_SUFFIXES lib64 lib lib/GB build/lib build/lib/Release
              PATHS ${FIND_GB_PATHS})
 
+find_library(GB_LIBRARY_DYNAMIC_RELEASE
+            NAMES GameBackbone-d
+            PATH_SUFFIXES lib64 lib lib/GB build/lib build/lib/Release
+            PATHS ${FIND_GB_PATHS})
 
-set(GB_LIBRARY_RELEASE ${GB_LIBRARY_STATIC_RELEASE})
+
+set(GB_LIBRARY_RELEASE ${GB_LIBRARY_DYNAMIC_RELEASE})
+set(GB_LIBRARY_DEBUG ${GB_LIBRARY_DYNAMIC_DEBUG})
 
 
-if (GB_LIBRARY_RELEASE)
+if (GB_LIBRARY_RELEASE OR GB_LIBRARY_DEBUG)
     # library found
     set(GB_FOUND TRUE)
 
+    # if both are found, set TGUI_LIBRARY to contain both
+    if (GB_LIBRARY_DEBUG and GB_LIBRARY_RELEASE)
+        set(GB_LIBRARY 
+            debug ${GB_LIBRARY_DEBUG} 
+            optimized ${GB_LIBRARY_RELEASE})
+    endif()
 
-    set(GB_LIBRARY       ${GB_LIBRARY_RELEASE})
-
+    # if only one debug/release variant is found, set the other to be equal to the found one
+    if (GB_LIBRARY_RELEASE AND NOT GB_LIBRARY_DEBUG)
+        set(GB_LIBRARY_DEBUG ${GB_LIBRARY_RELEASE})
+        set(GB_LIBRARY       ${GB_LIBRARY_RELEASE})
+    endif()
+    if (GB_LIBRARY_DEBUG AND NOT GB_LIBRARY_RELEASE)
+        set(GB_LIBRARY_RELEASE ${GB_LIBRARY_DEBUG})
+        set(GB_LIBRARY         ${GB_LIBRARY_DEBUG})
+    endif()
 endif()
 
 
-mark_as_advanced(GB_LIBRARY)
+mark_as_advanced(GB_LIBRARY
+                GB_LIBRARY_DEBUG
+                GB_LIBRARY_RELEASE
+                GB_LIBRARY_DYNAMIC_DEBUG
+                GB_LIBRARY_DYNAMIC_RELEASE)
 
-
+# error handling
 if(NOT GB_FOUND)
     # include directory or library not found
     set(FIND_GB_ERROR "Could NOT find GB")
 endif()
-
-
 if (NOT GB_FOUND)
     if(GB_FIND_REQUIRED)
         # fatal error
