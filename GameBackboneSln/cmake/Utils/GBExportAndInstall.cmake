55b9df1e76a45547d07898f7edddc584f9c7ab79
Ryan Lavin <lavinrp@gmail.com>
2019-03-03 04:54:25 -0500

    i164 - GB_Consumable: !BROKEN! Attempt to use CMakePackageConfigHelpers
    Attempt to use CMakePackageConfigHelpers to export GB for client use.
    Failing from "Unknown keywords given to CONFIGURE_PACKAGE_CONFIG_FILE()"

---
+++ GameBackboneSln/cmake/Utils/GBExportAndInstall.cmake
@@ -0,0 +1,47 @@
+#Installs and exports GameBackbone
+
+cmake_minimum_required (VERSION 3.8)
+
+set(CMAKE_INSTALL_GENERATION_DIR "${CMAKE_BINARY_DIR}/CmakeExport/")
+set(EXPORT_TARGETS_GENERATION_PATH "${CMAKE_INSTALL_GENERATION_DIR}/GameBackboneSharedTargets.cmake")
+set(GAMEBACKBONE_INSTALL_CMAKE_DIR "lib/cmake/GameBackbone")
+set(GAMEBACKBONE_PUBLIC_INCLUDE_INSTALL_DIR include)
+
+# configure config files
+include(CMakePackageConfigHelpers)
+configure_package_config_file (
+    "${CMAKE_SOURCE_DIR}/cmake/utils/GameBackboneConfig.cmake.in"
+    ${EXPORT_TARGETS_GENERATION_DIR}
+    INSTALL_DESTINATION ${GAMEBACKBONE_INSTALL_CMAKE_DIR}
+    PATH_VARS GAMEBACKBONE_INSTALL_CMAKE_DIR GAMEBACKBONE_PUBLIC_INCLUDE_INSTALL_DIR
+)
+
+write_basic_package_version_file (
+    ${EXPORT_TARGETS_GENERATION_DIR}
+    VERSION ${CMAKE_VERSION}
+    COMPATIBILITY ExactVersion # We can relax this after GB is more stable
+)
+
+# install GameBackbone lib
+install(
+    TARGETS GameBackbone
+    RUNTIME DESTINATION bin
+    LIBRARY DESTINATION lib
+    ARCHIVE DESTINATION lib
+)
+
+#install GameBackbone headers
+install(
+    DIRECTORY "${CMAKE_SOURCE_DIR}/GameBackbone/Include"
+    DESTINATION ${GAMEBACKBONE_PUBLIC_INCLUDE_INSTALL_DIR}
+)
+
+# install Cmake Files
+install (
+    EXPORT GameBackbonePublicTargets
+    DESTINATION ${GAMEBACKBONE_INSTALL_CMAKE_DIR}
+)
+install(
+    DIRECTORY ${EXPORT_TARGETS_GENERATION_DIR}
+    DESTINATION ${GAMEBACKBONE_INSTALL_CMAKE_DIR}
+)
