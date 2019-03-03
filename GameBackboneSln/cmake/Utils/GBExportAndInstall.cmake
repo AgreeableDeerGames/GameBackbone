0057557ba963d06441dcf99ae7db756869769ea7
Ryan Lavin <lavinrp@gmail.com>
2019-03-03 05:15:24 -0500

    i164 - GB_Consumable: fixed dumb path mistake and export set

--- GameBackboneSln/cmake/Utils/GBExportAndInstall.cmake
+++ GameBackboneSln/cmake/Utils/GBExportAndInstall.cmake
@@ -2,22 +2,29 @@
 
 cmake_minimum_required (VERSION 3.8)
 
-set(CMAKE_INSTALL_GENERATION_DIR "${CMAKE_BINARY_DIR}/CmakeExport/")
+set(CMAKE_INSTALL_GENERATION_DIR "${CMAKE_BINARY_DIR}/CmakeExport")
 set(EXPORT_TARGETS_GENERATION_PATH "${CMAKE_INSTALL_GENERATION_DIR}/GameBackboneSharedTargets.cmake")
 set(GAMEBACKBONE_INSTALL_CMAKE_DIR "lib/cmake/GameBackbone")
 set(GAMEBACKBONE_PUBLIC_INCLUDE_INSTALL_DIR include)
 
 # configure config files
 include(CMakePackageConfigHelpers)
+# configure_package_config_file (
+#     "${CMAKE_SOURCE_DIR}/cmake/Utils/GameBackboneConfig.cmake.in"
+#     ${EXPORT_TARGETS_GENERATION_DIR}
+#     INSTALL_DESTINATION ${GAMEBACKBONE_INSTALL_CMAKE_DIR}
+#     PATH_VARS GAMEBACKBONE_INSTALL_CMAKE_DIR GAMEBACKBONE_PUBLIC_INCLUDE_INSTALL_DIR
+# )
+
 configure_package_config_file (
-    "${CMAKE_SOURCE_DIR}/cmake/utils/GameBackboneConfig.cmake.in"
-    ${EXPORT_TARGETS_GENERATION_DIR}
+    "${CMAKE_SOURCE_DIR}/cmake/Utils/GameBackboneConfig.cmake.in"
+    "${CMAKE_INSTALL_GENERATION_DIR}/GameBackboneConfig.cmake"
     INSTALL_DESTINATION ${GAMEBACKBONE_INSTALL_CMAKE_DIR}
     PATH_VARS GAMEBACKBONE_INSTALL_CMAKE_DIR GAMEBACKBONE_PUBLIC_INCLUDE_INSTALL_DIR
 )
 
 write_basic_package_version_file (
-    ${EXPORT_TARGETS_GENERATION_DIR}
+    "${CMAKE_INSTALL_GENERATION_DIR}/GameBackboneConfigVersion.cmake"
     VERSION ${CMAKE_VERSION}
     COMPATIBILITY ExactVersion # We can relax this after GB is more stable
 )
@@ -25,6 +32,7 @@ write_basic_package_version_file (
 # install GameBackbone lib
 install(
     TARGETS GameBackbone
+    EXPORT GameBackbonePublicTargets
     RUNTIME DESTINATION bin
     LIBRARY DESTINATION lib
     ARCHIVE DESTINATION lib
