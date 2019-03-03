3fc1c4eddcb46fa357597fb8ddd14a9d8d33e0f5
Ryan Lavin <lavinrp@gmail.com>
2019-03-03 05:31:32 -0500

    i164-GB_Connsumable: GB may now be able to self install

--- GameBackboneSln/cmake/Utils/GBExportAndInstall.cmake
+++ GameBackboneSln/cmake/Utils/GBExportAndInstall.cmake
@@ -3,19 +3,11 @@
 cmake_minimum_required (VERSION 3.8)
 
 set(CMAKE_INSTALL_GENERATION_DIR "${CMAKE_BINARY_DIR}/CmakeExport")
-set(EXPORT_TARGETS_GENERATION_PATH "${CMAKE_INSTALL_GENERATION_DIR}/GameBackboneSharedTargets.cmake")
 set(GAMEBACKBONE_INSTALL_CMAKE_DIR "lib/cmake/GameBackbone")
 set(GAMEBACKBONE_PUBLIC_INCLUDE_INSTALL_DIR include)
 
 # configure config files
 include(CMakePackageConfigHelpers)
-# configure_package_config_file (
-#     "${CMAKE_SOURCE_DIR}/cmake/Utils/GameBackboneConfig.cmake.in"
-#     ${EXPORT_TARGETS_GENERATION_DIR}
-#     INSTALL_DESTINATION ${GAMEBACKBONE_INSTALL_CMAKE_DIR}
-#     PATH_VARS GAMEBACKBONE_INSTALL_CMAKE_DIR GAMEBACKBONE_PUBLIC_INCLUDE_INSTALL_DIR
-# )
-
 configure_package_config_file (
     "${CMAKE_SOURCE_DIR}/cmake/Utils/GameBackboneConfig.cmake.in"
     "${CMAKE_INSTALL_GENERATION_DIR}/GameBackboneConfig.cmake"
@@ -40,7 +32,7 @@ install(
 
 #install GameBackbone headers
 install(
-    DIRECTORY "${CMAKE_SOURCE_DIR}/GameBackbone/Include"
+    DIRECTORY "${CMAKE_SOURCE_DIR}/GameBackbone/Include/GameBackbone"
     DESTINATION ${GAMEBACKBONE_PUBLIC_INCLUDE_INSTALL_DIR}
 )
 
@@ -50,6 +42,9 @@ install (
     DESTINATION ${GAMEBACKBONE_INSTALL_CMAKE_DIR}
 )
 install(
-    DIRECTORY ${EXPORT_TARGETS_GENERATION_DIR}
-    DESTINATION ${GAMEBACKBONE_INSTALL_CMAKE_DIR}
+    FILES
+        ${CMAKE_INSTALL_GENERATION_DIR}/GameBackboneConfig.cmake
+        ${CMAKE_INSTALL_GENERATION_DIR}/GameBackboneConfigVersion.cmake
+    DESTINATION
+        ${GAMEBACKBONE_INSTALL_CMAKE_DIR}
 )
