0f7f0195f1ffd4a46f971a6769e7f6c9430b8fe6
Ryan Lavin <lavinrp@gmail.com>
2019-03-09 21:36:38 -0500

    i164 - GB_Consumable: TestGameBackboneConsumer now links to tgui and sf
    TestGameBackboneConsumer now finds and links to TGUI and SFML.
    Minor clean up and string changes.

--- GameBackboneSln/GameBackboneUnitTest/TestConsumer/main.cpp
+++ GameBackboneSln/GameBackboneUnitTest/TestConsumer/main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
+#include <GameBackbone/Core/GameRegion.h>
 
 int main(int, char**) {
     std::cout << "Hello, world!\n";
+
+    GB::GameRegion region;
+    region.behave(sf::Time::Zero);
 }
