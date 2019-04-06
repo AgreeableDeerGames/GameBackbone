4d84a2f173e4d345b2fb7039dc82784e69626330
gyurgyma <thetemplarofanubis@gmail.com>
2019-04-06 15:21:40 -0400

    i169-PolishExamples: Comment main. Commented DemoCoreEventController

--- GameBackboneSln/GameBackboneDemo/Source/DemoCoreEventController.cpp
+++ GameBackboneSln/GameBackboneDemo/Source/DemoCoreEventController.cpp
@@ -3,23 +3,19 @@
 
 using namespace EXE;
 
-
-// ctr / dtr
-
-
 /// <summary>
 /// Initializes a new instance of the <see cref="BackboneExeCoreEventController"/> class. Window width, height, and name are default.
 /// </summary>
 DemoCoreEventController::DemoCoreEventController() : CoreEventController("GameBackbone Demos") {
-	// init region and camera
+	// Initialize the MainMenuDemoRegion
 	mainMenuDemoRegion= new MainMenuDemoRegion(*window);
+	// Register setActiveRegion with mainMenuDemoRegion so that it can swap regions when needed
 	mainMenuDemoRegion->registerSetActiveRegionCB(std::bind(&DemoCoreEventController::setActiveRegion, this, std::placeholders::_1));
+	// Set mainMenuDemoRegion as the activeRegion on DemoCoreEventController
 	activeRegion = mainMenuDemoRegion;
-	camera.reset(sf::FloatRect(0, 0, (float)window->getSize().x, (float)window->getSize().y));
 
-	//initialize the mouse origin to the center of the window.
-	oldMouseX = window->getSize().x / 2;
-	oldMouseY = window->getSize().y / 2;
+	// Set the camera to the same as the window
+	camera.reset(sf::FloatRect(0, 0, (float)window->getSize().x, (float)window->getSize().y));
 }
 
 /// <summary>
@@ -40,50 +36,61 @@ bool DemoCoreEventController::handleCoreEvent(sf::Event & event) {
 	// Handle events not handled by the GUI
 	switch (event.type) {
 	case sf::Event::Closed:
+	{
+		// Close the window, thus closing the game.
 		window->close();
 		return true;
+	}
 	case sf::Event::MouseMoved:
 	{
-		// pan camera with mouse
-		/*window->setView(camera);
-		camera.move((float)(event.mouseMove.x - oldMouseX), (float)(event.mouseMove.y - oldMouseY));*/
-		oldMouseX = event.mouseMove.x;
-		oldMouseY = event.mouseMove.y;
-
+		// Get the pixel position and map it to coordinates
 		sf::Vector2i mousePos(event.mouseMove.x, event.mouseMove.y);
 		sf::Vector2f actualPosition = window->mapPixelToCoords(mousePos);
+		// Pass the event to the active region to handle
 		static_cast<DemoRegion*>(activeRegion)->handleMouseMove(actualPosition);
 		return true;
 	}
 	case sf::Event::MouseButtonPressed:
 	{
+		// Get the pixel position and map it to coordinates
 		sf::Vector2i mousePos(event.mouseButton.x, event.mouseButton.y);
 		sf::Vector2f actualPosition = window->mapPixelToCoords(mousePos);
+		// Pass the event to the active region to handle
 		static_cast<DemoRegion*>(activeRegion)->handleMouseClick(actualPosition, event.mouseButton.button);
 		return true;
 	}
 	case sf::Event::MouseWheelScrolled:
 	{
+		// Pass the event to the active region to handle
 		static_cast<DemoRegion*>(activeRegion)->handleWheelScroll(event.mouseWheelScroll.delta);
 		return true;
 	}
 	case sf::Event::KeyPressed:
 	{
+		// Pass the event to the active region to handle
 		static_cast<DemoRegion*>(activeRegion)->handleKeyPress(event.key);
 		return true;
 	}
 	case sf::Event::KeyReleased:
 	{
+		// Pass the event to the active region to handle
 		static_cast<DemoRegion*>(activeRegion)->handleKeyRelease(event.key);
 		return true;
 	}
 	case sf::Event::Resized:
+	{
+		// Reset the camera to the same as the window
 		camera.reset(sf::FloatRect(0, 0, (float)event.size.width, (float)event.size.height));
+		// Set the view on the window to be the reset camera
 		window->setView(camera);
+		// Set the view on the GUI to be the reset camera
 		activeRegion->getGUI()->setView(camera);
 		return true;
+	}
 	default:
+	{
 		return false;
+	}
 	}
 
 }
--- GameBackboneSln/GameBackboneDemo/Source/main.cpp
+++ GameBackboneSln/GameBackboneDemo/Source/main.cpp
@@ -3,7 +3,9 @@
 using namespace EXE;
 
 int main() {
+	// Initialize an instance of DemoCoreEventController
 	DemoCoreEventController controller;
+	// Start the loop on the DemoCoreEventController to start the Demo
 	controller.runLoop();
 
 	return 0;
