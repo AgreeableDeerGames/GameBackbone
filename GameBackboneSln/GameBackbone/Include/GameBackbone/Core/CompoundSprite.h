fc17fd79b7a490156c8cad5ce0ceb9736d10fd90
gyurgyma <thetemplarofanubis@gmail.com>
2019-12-07 19:09:53 -0500

    i194-PriorityCompoundSprite: DOES NOT COMPILE! Began SPPP with SFINAE and changing CS respectively.

--- GameBackboneSln/GameBackbone/Include/GameBackbone/Core/CompoundSprite.h
+++ GameBackboneSln/GameBackbone/Include/GameBackbone/Core/CompoundSprite.h
@@ -5,13 +5,35 @@
 #include <GameBackbone/Util/DllUtil.h>
 
 #include <SFML/Graphics/Drawable.hpp>
+#include <SFML/Graphics/Transformable.hpp>
 #include <SFML/Graphics/Sprite.hpp>
 
 #include <cstddef>
 #include <set>
+#include <utility>
 #include <vector>
 
+
 namespace GB {
+
+	// Base Trait for Drawable
+	template <class InType>
+	struct is_drawable : std::is_base_of<InType, sf::Drawable> {};
+	template <class InType>
+	inline constexpr bool is_drawable_v = is_drawable<InType>::value;
+
+	// Base Trait for Transformable
+	template <class InType>
+	struct is_transformable : std::is_base_of<InType, sf::Transformable> {};
+	template <class InType>
+	inline constexpr bool is_transformable_v = is_drawable<InType>::value;
+
+	// Base Trait for Updatable
+	template <class InType, class result_type = void>
+	struct is_updatable : std::is_base_of<InType, GB::Updatable> {};
+	template <class InType>
+	inline constexpr bool is_updatable_v = is_drawable<InType>::value;
+
 
 	/// <summary> Controls several sprites and animated sprites as one logical unit. </summary>
 	class libGameBackbone CompoundSprite : public Updatable, public sf::Drawable, public sf::Transformable {
@@ -30,17 +52,37 @@ namespace GB {
 		virtual ~CompoundSprite() = default;
 
 		// Component Getters
-		virtual sf::Sprite& getSpriteComponent(std::size_t componentIndex);
-		virtual AnimatedSprite& getAnimatedComponent(std::size_t componentIndex);
 		virtual std::size_t getSpriteComponentCount() const;
-		virtual std::size_t getAnimatedComponentCount() const;
 		virtual bool isEmpty() const;
 
 		// Add/Remove Components
-		virtual std::size_t addComponent(sf::Sprite component);
-		virtual std::size_t addComponent(AnimatedSprite component);
-		virtual void removeSpriteComponent(std::size_t componentIndex);
-		virtual void removeAnimatedComponent(std::size_t componentIndex);
+		template <
+			class Component,
+			std::enable_if_t<is_transformable_v<Component>, bool> = true, // must be transformable
+			std::enable_if_t<is_drawable_v<Component>, bool> = true // must be drawable
+		>
+		Component& addComponent(Component component) {
+			/* Moving the origin moves the drawn entity in the opposite direction.
+			 * Move the origin of the component to the current position of the CompoundSprite
+			 * and offset it by the origin of the CompoundSprite (this keeps things in the right place
+			 * if the origin has been changed before the component is added).
+			 *
+			 * Setting the position of the component to the position of the compound sprite will then
+			 * make the entity appear in the same place but rotate around the origin of the compound sprite
+			 * instead of its own origin.
+			 */
+			component.setOrigin(getPosition().x + getOrigin().x - component.getPosition().x, getPosition().y + getOrigin().y - component.getPosition().y);
+			component.setPosition(getPosition().x, getPosition().y);
+
+			// Add the component to the internalComponents
+			std::unique_ptr<InternalType>& returnValue = m_internalComponents.emplace_back(std::make_unique<InternalType>(std::move(component))); // Should this be perfect forwarding?
+		
+			// Return the place in the components vector that the new component was placed.
+			return static_cast<ComponentAdapter<Component>>(returnValue)->data;
+		}
+
+
+		virtual void removeComponent(std::size_t componentIndex);
 		virtual void clearComponents();
 
 		// Transformable API
@@ -65,7 +107,75 @@ namespace GB {
 		virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
 
 	private:
-		std::vector<sf::Sprite> m_components;
-		std::vector<AnimatedSprite> m_animatedComponents;
+		class VirtualTransformable : public sf::Transformable {
+		public:
+			virtual ~VirtualTransformable() = default;
+
+			virtual void setPosition(float x, float y) = 0;
+			virtual void setPosition(const sf::Vector2f& position) = 0;
+			virtual void setRotation(float angle) = 0;
+			virtual void setScale(float factorX, float factorY) = 0;
+			virtual void setScale(const sf::Vector2f& factors) = 0;
+			virtual void setOrigin(float x, float y) = 0;
+			virtual void setOrigin(const sf::Vector2f& origin) = 0;
+			virtual const sf::Vector2f& getPosition() const = 0;
+			virtual float getRotation() const = 0;
+			virtual const sf::Vector2f& getScale() const = 0;
+			virtual const sf::Vector2f& getOrigin() const = 0;
+			virtual void move(float offsetX, float offsetY) = 0;
+			virtual void move(const sf::Vector2f& offset) = 0;
+			virtual void rotate(float angle) = 0;
+			virtual void scale(float factorX, float factorY) = 0;
+			virtual void scale(const sf::Vector2f& factor) = 0;
+			virtual const sf::Transform& getTransform() const = 0;
+			virtual const sf::Transform& getInverseTransform() const = 0;
+		};
+
+		class InternalType : public sf::Drawable, public VirtualTransformable, public GB::Updatable {
+		public:
+			virtual ~InternalType() = default;
+		};
+		template <class Component>
+		class ComponentAdapter final : public InternalType {
+		public:
+			ComponentAdapter(Component x) : data(std::move(x)) { }
+			void draw(/*stuff*/) const override
+			{
+				data.draw(/*stuff*/);
+			}
+			void update(/*stuff*/) const override
+			{
+				if constexpr (is_updatable_v<Component>)
+				{
+					data.update(/*stuff*/);
+				}
+			}
+
+			// Transformable
+			void setPosition(float x, float y) override { data.setPosition(x, y); }
+			void setPosition(const sf::Vector2f& position) override { data.setPosition(position); }
+			void setRotation(float angle) override { data.setRotation(angle); }
+			void setScale(float factorX, float factorY) override { data.setScale(factorX, factorY); }
+			void setScale(const sf::Vector2f& factors) override { data.setScale(factors); }
+			void setOrigin(float x, float y) override { data.setOrigin(x, y); }
+			void setOrigin(const sf::Vector2f& origin) override { data.setOrigin(origin); }
+			const sf::Vector2f& getPosition() const override { data.getPosition(); }
+			float getRotation() const override { data.getRotation(); }
+			const sf::Vector2f& getScale() const override { data.getScale(); }
+			const sf::Vector2f& getOrigin() const override { data.getOrigin(); }
+			void move(float offsetX, float offsetY) override { data.move(offsetX, offsetY); }
+			void move(const sf::Vector2f& offset) override { data.move(offset); }
+			void rotate(float angle) override { data.rotate(angle); }
+			void scale(float factorX, float factorY) override { data.scale(factorX, factorY); }
+			void scale(const sf::Vector2f& factor) override { data.scale(factor); }
+			const sf::Transform& getTransform() const override { data.getTransform(); }
+			const sf::Transform& getInverseTransform() const override { data.getInverseTransform(); }
+
+			Component data;
+		};
+
+		std::vector<std::unique_ptr<InternalType>> m_internalComponents;
+
+			
 	};
 }
--- GameBackboneSln/GameBackbone/Source/Core/CompoundSprite.cpp
+++ GameBackboneSln/GameBackbone/Source/Core/CompoundSprite.cpp
@@ -80,97 +80,16 @@ CompoundSprite::CompoundSprite(sf::Vector2f position){
 }
 
 /// <summary>
-/// Returns the Sprite component at the provided index.
-/// Throws std::out_of_range exception if the index is out of bounds.
-/// </summary>
-/// <param name="componentIndex"> </param>
-/// <return> sf::Sprite& </return>
-sf::Sprite& CompoundSprite::getSpriteComponent(std::size_t componentIndex) {
-	return m_components.at(componentIndex);
-}
-
-/// <summary>
-/// Returns the AnimatedSprite component at the provided index.
-/// Throws std::out_of_range exception if the index is out of bounds.
-/// </summary>
-/// <param name="componentIndex"> </param>
-/// <return> sf::Sprite& </return>
-AnimatedSprite& CompoundSprite::getAnimatedComponent(std::size_t componentIndex) {
-	return m_animatedComponents.at(componentIndex);
-}
-
-/// <summary>
 /// Gets the count of Sprite components.
 /// </summary>
 /// <return> The count of sprite components. </return>
 std::size_t CompoundSprite::getSpriteComponentCount() const {
-	return m_components.size();
-}
-
-/// <summary>
-/// Gets the count of AnimatedSprite components.
-/// </summary>
-/// <return> The count of AnimatedSprite components. </return>
-std::size_t CompoundSprite::getAnimatedComponentCount() const {
-	return m_animatedComponents.size();
+	return m_internalComponents.size();
 }
 
 /// <summary>True if this CompoundSprite holds no components. False otherwise.</summary>
 bool CompoundSprite::isEmpty() const {
-	return m_components.empty() && m_animatedComponents.empty();
-}
-
-/// <summary>
-/// Adds a Sprite component to the CompoundSprite.
-/// </summary>
-/// <param name="component"> The component to add to the CompoundSprite. </param>
-/// <return> The index of the added sprite within the CompoundSprite. </return>
-std::size_t CompoundSprite::addComponent(sf::Sprite component) {
-
-	/* Moving the origin moves the drawn entity in the opposite direction.
-	 * Move the origin of the component to the current position of the CompoundSprite
-	 * and offset it by the origin of the CompoundSprite (this keeps things in the right place
-	 * if the origin has been changed before the component is added).
-	 *
-	 * Setting the position of the component to the position of the compound sprite will then 
-	 * make the entity appear in the same place but rotate around the origin of the compound sprite
-	 * instead of its own origin.
-	 */
-	component.setOrigin(getPosition().x + getOrigin().x - component.getPosition().x, getPosition().y + getOrigin().y - component.getPosition().y);
-	component.setPosition(getPosition().x, getPosition().y);
-
-	// Add the component the vector of Sprite components
-	m_components.emplace_back(std::move(component));
-
-	// Return the place in the components vector that the new component was placed.
-	return m_components.size() - 1;
-}
-
-/// <summary>
-/// Adds an AnimatedSprite component to the CompoundSprite.
-/// Sets the animated sprite to be updated when the compound sprite is updated.
-/// </summary>
-/// <param name="component"> The component to add to the CompoundSprite. </param>
-/// <return> The index of the added AnimatedSprite within the CompoundSprite. </return>
-std::size_t CompoundSprite::addComponent(AnimatedSprite component) {
-
-	/* Moving the origin moves the drawn entity in the opposite direction.
-	 * Move the origin of the component to the current position of the CompoundSprite
-	 * and offset it by the origin of the CompoundSprite (this keeps things in the right place
-	 * if the origin has been changed before the component is added).
-	 *
-	 * Setting the position of the component to the position of the compound sprite will then
-	 * make the entity appear in the same place but rotate around the origin of the compound sprite
-	 * instead of its own origin.
-	 */
-	component.setOrigin(getPosition().x + getOrigin().x - component.getPosition().x, getPosition().y + getOrigin().y - component.getPosition().y);
-	component.setPosition(getPosition().x, getPosition().y);
-
-	// Add the component the vector of AnimatedSprite components
-	m_animatedComponents.emplace_back(std::move(component));
-
-	// Return the place in the components vector that the new component was placed.
-	return m_animatedComponents.size() - 1;
+	return m_internalComponents.empty();
 }
 
 /// <summary>
@@ -179,23 +98,9 @@ std::size_t CompoundSprite::addComponent(AnimatedSprite component) {
 /// This invalidates all indices returned by addComponent(sf::sprite)
 /// </summary>
 /// <param name="component">The component to remove from the CompoundSprite</param>
-void CompoundSprite::removeSpriteComponent(std::size_t componentIndex) {
-	if (componentIndex < m_components.size()) {
-		m_components.erase(m_components.begin() + componentIndex);
-	} else {
-		throw std::out_of_range("Component not in CompoundSprite");
-	}
-}
-
-/// <summary>
-/// Removes the AnimatedSprite component at the passed index from the CompoundSprite.
-/// Throws std::out_of_range exception if the component index is out of bounds.
-/// This invalidates all indices returned by addComponent(AnimatedSprite)
-/// </summary>
-/// <param name="component">The component to remove from the CompoundSprite</param>
-void CompoundSprite::removeAnimatedComponent(std::size_t componentIndex) {
-	if (componentIndex < m_animatedComponents.size()) {
-		m_animatedComponents.erase(m_animatedComponents.begin() + componentIndex);
+void CompoundSprite::removeComponent(std::size_t componentIndex) {
+	if (componentIndex < m_internalComponents.size()) {
+		m_internalComponents.erase(m_internalComponents.begin() + componentIndex);
 	} else {
 		throw std::out_of_range("Component not in CompoundSprite");
 	}
@@ -205,8 +110,7 @@ void CompoundSprite::removeAnimatedComponent(std::size_t componentIndex) {
 /// Removes all components from the compound sprite
 /// </summary>
 void CompoundSprite::clearComponents() {
-	m_components.clear();
-	m_animatedComponents.clear();
+	m_internalComponents.clear();
 }
 
 /// <summary>
@@ -240,8 +144,7 @@ void CompoundSprite::setRotation(float angle) {
 	};
 
 	// Apply the rotateFunction to all components
-	std::for_each(std::begin(m_components), std::end(m_components), setRotationFunction);
-	std::for_each(std::begin(m_animatedComponents), std::end(m_animatedComponents), setRotationFunction);
+	std::for_each(std::begin(m_internalComponents), std::end(m_internalComponents), setRotationFunction);
 
 	// Update the position of the CompoundSprite as a whole
 	sf::Transformable::setRotation(angle);
@@ -259,8 +162,7 @@ void CompoundSprite::setScale(float factorX, float factorY) {
 	};
 
 	// Apply the setScaleFunction to all components
-	std::for_each(std::begin(m_components), std::end(m_components), setScaleFunction);
-	std::for_each(std::begin(m_animatedComponents), std::end(m_animatedComponents), setScaleFunction);
+	std::for_each(std::begin(m_internalComponents), std::end(m_internalComponents), setScaleFunction);
 
 	// Update the position of the CompoundSprite as a whole
 	sf::Transformable::setScale(factorX, factorY);
@@ -292,8 +194,7 @@ void CompoundSprite::setOrigin(float x, float y) {
 	};
 
 	// update the origin of all components
-	std::for_each(std::begin(m_components), std::end(m_components), setOriginFunction);
-	std::for_each(std::begin(m_animatedComponents), std::end(m_animatedComponents), setOriginFunction);
+	std::for_each(std::begin(m_internalComponents), std::end(m_internalComponents), setOriginFunction);
 
 	// Update the origin of the CompoundSprite as a whole
 	sf::Transformable::setOrigin(x, y);
@@ -320,8 +221,7 @@ void CompoundSprite::move(float offsetX, float offsetY) {
 	};
 
 	// apply the rotateFunction to all components
-	std::for_each(std::begin(m_components), std::end(m_components), moveFunction);
-	std::for_each(std::begin(m_animatedComponents), std::end(m_animatedComponents), moveFunction);
+	std::for_each(std::begin(m_internalComponents), std::end(m_internalComponents), moveFunction);
 
 	// update the position of the CompoundSprite as a whole
 	sf::Transformable::move(offsetX, offsetY);
@@ -347,8 +247,7 @@ void CompoundSprite::rotate(float angle) {
 	};
 
 	// Apply the rotateFunction to all components
-	std::for_each(std::begin(m_components), std::end(m_components), rotateFunction);
-	std::for_each(std::begin(m_animatedComponents), std::end(m_animatedComponents), rotateFunction);
+	std::for_each(std::begin(m_internalComponents), std::end(m_internalComponents), rotateFunction);
 
 	// Update the position of the CompoundSprite as a whole
 	sf::Transformable::rotate(angle);
@@ -366,8 +265,7 @@ void CompoundSprite::scale(float factorX, float factorY) {
 	};
 
 	// Apply the scaleFunction to all components
-	std::for_each(std::begin(m_components), std::end(m_components), scaleFunction);
-	std::for_each(std::begin(m_animatedComponents), std::end(m_animatedComponents), scaleFunction);
+	std::for_each(std::begin(m_internalComponents), std::end(m_internalComponents), scaleFunction);
 
 	// Update the position of the CompoundSprite as a whole
 	sf::Transformable::scale(factorX, factorY);
@@ -387,8 +285,8 @@ void CompoundSprite::scale(const sf::Vector2f& factor) {
 /// <param name="elapsedTime">The elapsed time.</param>
 void CompoundSprite::update(sf::Int64 elapsedTime) {
 	// Forward the update to each component
-	for (auto& animatedComponent : m_animatedComponents) {
-		animatedComponent.update(elapsedTime);
+	for (auto& component : m_internalComponents) {
+		component->update(elapsedTime);
 	}
 }
 
@@ -404,7 +302,6 @@ void CompoundSprite::draw(sf::RenderTarget& target, sf::RenderStates states) con
 	};
 
 	// apply drawFunction to all components
-	std::for_each(std::begin(m_components), std::end(m_components), drawFunction);
-	std::for_each(std::begin(m_animatedComponents), std::end(m_animatedComponents), drawFunction);
+	std::for_each(std::begin(m_internalComponents), std::end(m_internalComponents), drawFunction);
 }
 
