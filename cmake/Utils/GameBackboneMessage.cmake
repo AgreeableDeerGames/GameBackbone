c221c567f416bbd39f5bbc71b48f71a2e6577bb4
Ryan Lavin <lavinrp@gmail.com>
2020-04-05 02:41:05 -0400

    i138 - GitHubActions: run clang tidy

--- cmake/Utils/GameBackboneMessage.cmake
+++ cmake/Utils/GameBackboneMessage.cmake
@@ -8,3 +8,7 @@ endfunction()
 function(gamebackbone_warning msg)
 	message(WARNING "[GameBackbone] ${msg}")
 endfunction()
+
+function(gamebackbone_fatal_error msg)
+	message(FATAL_ERROR "[GameBackbone] ${msg}")
+endfunction()
