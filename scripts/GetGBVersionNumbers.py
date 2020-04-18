50063c41249ba84599b0676bf62b7281717aa0aa
Ryan Lavin <lavinrp@gmail.com>
2020-04-18 16:37:18 -0400

    i138 - GitHubActions: Can now update GB versions with script

--- scripts/GetGBVersionNumbers.py
+++ scripts/GetGBVersionNumbers.py
@@ -36,7 +36,7 @@ def set_gb_version(backbone_top_cmake_path, new_version):
     """
     Set the game backbone version number by searching the top level CMake file
     """
-    with open(backbone_top_cmake_path, 'w+') as file:
+    with open(backbone_top_cmake_path, 'r+') as file:
         cmake_text = file.read()
         regex_result = re.sub(gb_version_regex, repl=r"\"GameBackboneProject\" VERSION " + new_version, string=cmake_text)
         file.seek(0)
@@ -47,7 +47,7 @@ def set_doxygen_version(doxyfile_path, new_version):
     """
     Set the game backbone version number in a Doxyfile
     """
-    with open(doxyfile_path, 'w+') as file:
+    with open(doxyfile_path, 'r+') as file:
         doxyfile_text = file.read()
         regex_result = re.sub(doxyfile_gb_versioin_regex, repl=r"PROJECT_NUMBER         = " + new_version, string=doxyfile_text)
         file.seek(0)
@@ -58,7 +58,7 @@ def set_install_consumer_gb_version(install_consumer_cmake_path, new_version):
     """
     Set the game backbone version number in the game backbone install consumer
     """
-    with open(install_consumer_cmake_path, 'w+') as file:
+    with open(install_consumer_cmake_path, 'r+') as file:
         cmake_text = file.read()
         replacement_text = f"find_package(GameBackbone {new_version} REQUIRED)"
         regex_result = re.sub(doxyfile_gb_versioin_regex, repl=replacement_text, string=cmake_text)
