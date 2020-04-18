da3a056705da586a66de29e701004ecbdc25cfd9
Ryan Lavin <lavinrp@gmail.com>
2020-04-18 16:25:33 -0400

    i138 - GitHubActions: fixes to script to update GB version number

--- scripts/SetGBVersionNumbers.py
+++ scripts/SetGBVersionNumbers.py
@@ -8,10 +8,10 @@ import os
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Updates GB VCPKG port to new version")
-    parser.add_argument("--version", type=str, help="New GB version", dest="version")
-    parser.add_argument("--gb_path", type=str, help="path to the top level folder of the GB repo", dest="gb_path")
+    parser.add_argument("--version", type=str, help="New GB version", dest="version", required=True)
+    parser.add_argument("--gb_path", type=str, help="path to the top level folder of the GB repo", dest="gb_path", required=True)
 
     args = parser.parse_args()
     set_gb_version(os.path.join(args.gb_path, "CMakeLists.txt"), args.version)
-    set_doxygen_version(os.pardir.join(args.gb_path, "Doxyfile"), args.version)
-    set_install_consumer_gb_version(os.pardir.join(args.gb_path, "Tests", "InstallConsumer", "CMakeLists.txt"), args.version)
+    set_doxygen_version(os.path.join(args.gb_path, "Doxyfile"), args.version)
+    set_install_consumer_gb_version(os.path.join(args.gb_path, "Tests", "InstallConsumer", "CMakeLists.txt"), args.version)
