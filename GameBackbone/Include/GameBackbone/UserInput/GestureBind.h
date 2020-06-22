3243578349f93ee2af789cd164736bfb79218a6d
Ryan Lavin <lavinrp@gmail.com>
2020-06-21 21:16:20 -0400

    Revert "i234 - InputRouter: GestureBind isDone and reset"
    This reverts commit 546baa83e07a4d0fbef275161d2996cb346a2733.

--- GameBackbone/Include/GameBackbone/UserInput/GestureBind.h
+++ GameBackbone/Include/GameBackbone/UserInput/GestureBind.h
@@ -50,7 +50,6 @@ namespace GB
 
 		// TODO: this should probably be a different name so as to not be confused with InputHandler
 		HandleEvenResult handleEvent(sf::Int64 elapsedTime, const sf::Event& event); // override;
-		void reset();
 
 		const std::vector<sf::Event>& getGesture() const;
 		const std::function<void()>& getAction() const;
@@ -58,7 +57,6 @@ namespace GB
 		const std::string& getName() const;
 		sf::Int64 getMaxTimeBetweenInputs() const;
 		EndType getEndType() const;
-		bool isDone() const;
 
 		void setGesture(std::vector<sf::Event> gesture);
 		void setAction(std::function<void()> action);
@@ -78,6 +76,5 @@ namespace GB
 		sf::Int64 m_maxTimeBetweenInputs;
 		EndType m_endType;
 		std::size_t m_position;
-		bool m_isDone;
 	};
 }
--- GameBackbone/Source/UserInput/GestureBind.cpp
+++ GameBackbone/Source/UserInput/GestureBind.cpp
@@ -27,11 +27,6 @@ GestureBind::GestureBind(
 
 GestureBind::HandleEvenResult GestureBind::handleEvent(sf::Int64 elapsedTime, const sf::Event& event)
 {
-	if (m_isDone)
-	{
-		return HandleEvenResult::Done;
-	}
-
 	if (compareEvents(getNextEvent(), event) && elapsedTime < m_maxTimeBetweenInputs )
 	{
 		++m_position;
@@ -45,12 +40,6 @@ GestureBind::HandleEvenResult GestureBind::handleEvent(sf::Int64 elapsedTime, co
 
 	m_position = 0;
 	return HandleEvenResult::Reset;
-}
-
-void GestureBind::reset()
-{
-	m_isDone = false;
-	m_position = 0;
 }
 
 const std::vector<sf::Event>& GestureBind::getGesture() const
@@ -81,11 +70,6 @@ sf::Int64 GestureBind::getMaxTimeBetweenInputs() const
 GestureBind::EndType GestureBind::getEndType() const
 {
 	return m_endType;
-}
-
-bool GestureBind::isDone() const
-{
-	return m_isDone;
 }
 
 void GestureBind::setGesture(std::vector<sf::Event> gesture)
