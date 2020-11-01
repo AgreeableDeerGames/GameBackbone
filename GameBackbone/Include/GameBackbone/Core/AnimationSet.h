d46f5f73ac3998c64c508bd5881976f8af702798
Ryan Lavin <lavinrp@gmail.com>
2020-10-31 22:42:57 -0500

    i239 - header_comments: Update AnimationSet comments to new system

--- GameBackbone/Include/GameBackbone/Core/AnimationSet.h
+++ GameBackbone/Include/GameBackbone/Core/AnimationSet.h
@@ -11,10 +11,10 @@
 
 namespace GB {
 
-    /// <summary>Collection of texture subsections</summary>
+    /// @brief Collection of texture subsections
     using Animation = std::vector<sf::IntRect>;
 
-    /// <summary> Creates and owns a collection of animations.</summary>
+    /// @brief Creates and owns a collection of animations.
     class libGameBackbone AnimationSet {
     private:
         using InternalContainer = std::vector<Animation>;
@@ -26,43 +26,107 @@ namespace GB {
         using reverse_iterator = InternalContainer::reverse_iterator;
         using const_reverse_iterator = InternalContainer::const_reverse_iterator;
 
-        // Pointer type
-        /// <summary>shared_ptr to AnimationSet.</summary>
+        /// @brief shared_ptr to AnimationSet.
         using Ptr = std::shared_ptr<AnimationSet>;
 
-        // ctrs / dtr
+        /// @brief Construct an empty AnimationSet
         AnimationSet() = default;
+
+        /// @brief Construct a new AnimationSet object.
+        ///     The provided animations will all be added to the animation set.
+        /// @param animations The animations to add.
         AnimationSet(std::vector<Animation> animations);
+
+        /// @brief Copy construct an AnimationSet
+        /// @param  The AnimationSet to copy
         AnimationSet(const AnimationSet&) = default;
+
+        /// @brief Move construct an AnimationSet
+        /// @param  The AnimationSet to move
         AnimationSet(AnimationSet&&) = default;
+
+        /// @brief Copy assign an AnimationSet
+        /// @param  The AnimationSet to copy
+        /// @return This
         AnimationSet& operator=(const AnimationSet&) = default;
+
+        /// @brief Move assign an AnimationSet
+        /// @param  The AnimationSet to move
+        /// @return this
         AnimationSet& operator=(AnimationSet&&) = default;
+
+        /// @brief Destroy the AnimationSet
         virtual ~AnimationSet() = default;
 
-        // add / remove
+        /// @brief Adds the animation to the AnimationSet.
+        /// @param animation The animation to add.
         void addAnimation(Animation animation);
+
+        /// @brief Removes an animation from the AnimationSet.
+        /// @param animation The animation to remove.
         void eraseAnimation(const_iterator animation);
+
+        /// @brief Removes a range of animations form the AnimationSet.
+        /// @param first The first animation to erase.
+        /// @param last The last animation to erase.
         void eraseAnimations(const_iterator first, const_iterator last);
+
+        /// @brief Remove all animations from the AnimationSet.
         void clearAnimations();
 
-        // accessor / mutator
+        /// @brief Access the animation at the given index.
+        /// @param animationIndex The index of the animation to return.
+        /// @return The animation at the provided index.
+        /// @note Does no runtime safety checks
         const Animation& operator[](std::size_t animationIndex) const noexcept;
+
+        /// @brief Access the animation at the given index.
+        /// @param animationIndex The index of the animation to return. 
+        /// @return The animation at the provided index. 
+        /// @throws Throws out_of_range exception if the provided index is greater than the
+        ///     number of animations stored by the AnimationSet.
         const Animation& at(std::size_t animationIndex) const;
+
+        /// @brief Returns the number of Animations stored by the AnimationSet.
         std::size_t getSize() const;
+
+        /// @brief Returns true if the AnimationSet holds no animations. Returns false otherwise.
         bool isEmpty() const;
 
-        // Iterator functions
+        /// @brief Iterator to the beginning of the AnimationSet.
         iterator begin();
+
+        /// @brief Const iterator to the beginning of the AnimationSet.
         const_iterator begin() const noexcept;
+
+        /// @brief Const iterator to the beginning of the AnimationSet.
         const_iterator cbegin() const noexcept;
+
+        /// @brief Iterator to the end of the AnimationSet.
         iterator end();
+
+        /// @brief Const iterator to the end of the AnimationSet.
         const_iterator end() const noexcept;
+
+        /// @brief Const iterator to the end of the AnimationSet.
         const_iterator cend() const noexcept;
+
+        /// @brief Reverse iterator to the beginning of the AnimationSet.
         reverse_iterator rbegin();
+
+        /// @brief Reverse const iterator to the beginning of the AnimationSet.
         const_reverse_iterator rbegin() const noexcept;
+
+        /// @brief Reverse const iterator to the beginning of the AnimationSet.
         const_reverse_iterator crbegin() const noexcept;
+
+        /// @brief Reverse iterator to the end of the AnimationSet.
         reverse_iterator rend();
+
+        /// @brief Reverse const iterator to the end of the AnimationSet.
         const_reverse_iterator rend() const noexcept;
+
+        /// @brief Reverse const iterator to the end of the AnimationSet.
         const_reverse_iterator crend() const noexcept;
 
     private:
--- GameBackbone/Source/Core/AnimationSet.cpp
+++ GameBackbone/Source/Core/AnimationSet.cpp
@@ -8,161 +8,87 @@
 #include <vector>
 
 using namespace GB;
-/// <summary>
-/// Construct a new AnimationSet object.
-/// The provided animations will all be added to the animation set.
-/// </summary>
-/// <param name="animations"> The animations to add. </param>
+
 AnimationSet::AnimationSet(std::vector<Animation> animations) {
     this->animations = std::move(animations);
 }
 
-/// <summary>
-/// Adds the animation to the AnimationSet.
-/// </summary>
-/// <param name="animation"> The animation to add. </param>
 void AnimationSet::addAnimation(Animation animation) {
     animations.emplace_back(std::move(animation));
 }
 
-/// <summary>
-/// Removes an animation from the AnimationSet.
-/// </summary>
-/// <param name="animation"> The animation to remove. </param>
 void AnimationSet::eraseAnimation(const_iterator animation) {
     animations.erase(animation);
 }
 
-/// <summary>
-/// Removes a range of animations form the AnimationSet.
-/// </summary>
-/// <param name="first"> The first animation to erase. </param>
-/// <param name="last"> The last animation to erase. </param>
 void AnimationSet::eraseAnimations(const_iterator first, const_iterator last) {
     animations.erase(first, last);
 }
 
-/// <summary>
-/// Remove all animations from the AnimationSet.
-/// </summary>
 void AnimationSet::clearAnimations() {
     animations.clear();
 }
 
-/// <summary>
-/// Access the animation at the given index.
-/// </summary>
-/// <param name="animationIndex"> The index of the animation to return. </param>
-/// <return> The animation at the provided index. </return>
 const Animation& AnimationSet::operator[](std::size_t animationIndex) const noexcept {
     return animations[animationIndex];
 }
 
-/// <summary>
-/// Access the animation at the given index.
-/// Throws out_of_range exception if the provided index is greater than the
-/// number of animations stored by the AnimationSet.
-/// </summary>
-/// <param name="animationIndex"> The index of the animation to return. </param>
-/// <return> The animation at the provided index. </return>
 const Animation& AnimationSet::at(std::size_t animationIndex) const {
     return animations.at(animationIndex);
 }
 
-/// <summary>
-/// Returns the number of Animations stored by the AnimationSet.
-/// </summary>
 std::size_t AnimationSet::getSize() const {
     return animations.size();
 }
 
-/// <summary>
-/// Returns true if the AnimationSet holds no animations. Returns false otherwise.
-/// </summary>
 bool AnimationSet::isEmpty() const {
     return animations.empty();
 }
 
-/// <summary>
-/// Iterator to the beginning of the AnimationSet.
-/// </summary>
 AnimationSet::iterator AnimationSet::begin() {
     return animations.begin();
 }
 
-/// <summary>
-/// Const iterator to the beginning of the AnimationSet.
-/// </summary>
 AnimationSet::const_iterator AnimationSet::begin() const noexcept{
     return animations.begin();
 }
 
-/// <summary>
-/// Const iterator to the beginning of the AnimationSet.
-/// </summary>
 AnimationSet::const_iterator AnimationSet::cbegin() const noexcept{
     return animations.cbegin();
 }
 
-/// <summary>
-/// Iterator to the end of the AnimationSet.
-/// </summary>
 AnimationSet::iterator AnimationSet::end() {
     return animations.end();
 }
 
-/// <summary>
-/// Const iterator to the end of the AnimationSet.
-/// </summary>
 AnimationSet::const_iterator AnimationSet::end() const noexcept{
     return animations.end();
 }
 
-/// <summary>
-/// Const iterator to the end of the AnimationSet.
-/// </summary>
 AnimationSet::const_iterator AnimationSet::cend() const noexcept{
     return animations.cend();
 }
 
-/// <summary>
-/// Reverse iterator to the beginning of the AnimationSet.
-/// </summary>
 AnimationSet::reverse_iterator AnimationSet::rbegin(){
     return animations.rbegin();
 }
 
-/// <summary>
-/// Reverse const iterator to the beginning of the AnimationSet.
-/// </summary>
 AnimationSet::const_reverse_iterator AnimationSet::rbegin() const noexcept{
     return animations.rbegin();
 }
 
-/// <summary>
-/// Reverse const iterator to the beginning of the AnimationSet.
-/// </summary>
 AnimationSet::const_reverse_iterator AnimationSet::crbegin() const noexcept{
     return animations.crbegin();
 }
 
-/// <summary>
-/// Reverse iterator to the end of the AnimationSet.
-/// </summary>
 AnimationSet::reverse_iterator AnimationSet::rend(){
     return animations.rend();
 }
 
-/// <summary>
-/// Reverse const iterator to the end of the AnimationSet.
-/// </summary>
 AnimationSet::const_reverse_iterator AnimationSet::rend() const noexcept{
     return animations.rend();
 }
 
-/// <summary>
-/// Reverse const iterator to the end of the AnimationSet.
-/// </summary>
 AnimationSet::const_reverse_iterator AnimationSet::crend() const noexcept{
     return animations.crend();
 }
