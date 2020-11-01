4fc2b991af19dd3187cb0b5bab3c4f3ae96d3a1c
Ryan Lavin <lavinrp@gmail.com>
2020-11-01 02:07:37 -0500

    i239 - header_comments:Update BasicGameRegion comments to use new system

--- GameBackbone/Include/GameBackbone/Core/BasicGameRegion.h
+++ GameBackbone/Include/GameBackbone/Core/BasicGameRegion.h
@@ -9,27 +9,50 @@
 #include <memory>
 
 namespace GB {
-	/// <summary> Base class meant to be inherited. Controls game logic and actors for a specific time or space in game. </summary>
+	
+	/// @brief Base class meant to be inherited. Controls game logic and actors for a specific time or space in game.
 	class libGameBackbone BasicGameRegion : public sf::Drawable, public Updatable {
 	public:
-		/// <summary>shared_ptr to BasicGameRegion</summary>
+		
+		/// @brief shared_ptr to BasicGameRegion
 		using Ptr = std::shared_ptr<BasicGameRegion>;
 
+		/// @brief Initializes a new instance of the BasicGameRegionclass.
 		BasicGameRegion();
+
+		/// @brief Copy construct a BasicGameRegion
+		/// @param  The BasicGameRegion to copy
 		BasicGameRegion(const BasicGameRegion&) = default;
+
+		/// @brief Copy assign a BasicGameRegion
+		/// @param  The BasicGameRegion to copy
+		/// @return This
 		BasicGameRegion& operator=(const BasicGameRegion&) = default;
+
+		/// @brief Move Construct a BasicGameRegion
+		/// @param The BasicGameRegion to move
+		/// @return This
 		BasicGameRegion(BasicGameRegion&&) noexcept = default;
+
+		/// @brief Move assign a basicGameRegion
+		/// @param The BasicGameRegion to move
+		/// @return this
 		BasicGameRegion& operator=(BasicGameRegion&&) noexcept = default;
+
+		/// @brief Destroy this BasicGameRegion
 		virtual ~BasicGameRegion() = default;
 
+		/// @brief Gets the game region that should become active after the next update of this one.
 		BasicGameRegion& getNextRegion();
+
+		/// @brief The region that should become active after the next update of this one.
 		const BasicGameRegion& getNextRegion() const;
+
+		/// @brief Sets the BasicGameRegion that should become active after the next update of this one.
+		/// @param nextRegion The region that should become active.
 		void setNextRegion(BasicGameRegion& nextRegion);
 
-		/// <summary>
-		/// Implements Updatable::update as a no-op.
-		/// </summary>
-		/// <param name="elapsedTime"> </param>
+		/// @brief Implements Updatable::update as a no-op.
 		void update(sf::Int64 /*elapsedTime*/) override {}
 
 	private:
--- GameBackbone/Source/Core/BasicGameRegion.cpp
+++ GameBackbone/Source/Core/BasicGameRegion.cpp
@@ -8,33 +8,18 @@
 
 using namespace GB;
 
-/// <summary>
-/// Initializes a new instance of the <see cref="BasicGameRegion"/> class.
-/// </summary>
 BasicGameRegion::BasicGameRegion() : m_nextRegion(*this) {}
 
-/// <summary>
-/// Gets the game region that should become active after the next update of this one.
-/// </summary>
-/// <returns>The region that should become active after the next update of this one.</returns>
 BasicGameRegion& BasicGameRegion::getNextRegion()
 {
 	return m_nextRegion;
 }
 
-/// <summary>
-/// The game region that should become active after the next update of this one.
-/// </summary>
-/// <returns>The region that should become active after the next update of this one.</returns>
 const BasicGameRegion& BasicGameRegion::getNextRegion() const
 {
 	return m_nextRegion;
 }
 
-/// <summary>
-/// Sets the BasicGameRegion that should become active after the next update of this one.
-/// </summary>
-/// <param name="nextRegion">The region that should become active after the next update of this one.</param>
 void BasicGameRegion::setNextRegion(BasicGameRegion& nextRegion)
 {
 	m_nextRegion = nextRegion;
