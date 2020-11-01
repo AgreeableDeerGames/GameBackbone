4c9704f045b5f70ff9abf39cf0e89b4eb41c0805
Ryan Lavin <lavinrp@gmail.com>
2020-10-31 22:04:17 -0500

    i239 - header_comments: Update utils comments to new system

--- GameBackbone/Include/GameBackbone/Util/RandGen.h
+++ GameBackbone/Include/GameBackbone/Util/RandGen.h
@@ -7,26 +7,41 @@
 
 
 namespace GB {
-
-	/// <summary>
-	/// This class is a shallow wrapper for std::random.
-	/// To use this class, make an instance of RandGen with a given seed. Call uniDist anytime you need a random number.
-	/// </summary>
+	
+	/// @brief This class is a shallow wrapper for std::random.
+	///		To use this class, make an instance of RandGen with a given seed. Call uniDist anytime you need a random number.
 	class libGameBackbone RandGen {
 	public:
+
+		/// @brief Initializes a new instance of the RandGen class with a random seed.
 		RandGen();
+
+		/// @brief Initializes a new instance of the RandGen class.
+		/// @param seed The seed for the Generator.
 		explicit RandGen(std::string* seed);
 
 		RandGen(const RandGen& generator) = default;
 		RandGen(RandGen&& generator) = default;
 		RandGen& operator= (const RandGen& generator) = default;
 		RandGen& operator= (RandGen&& generator) = default;
+
+		/// @brief Destroys an instance of RandGen
 		~RandGen();
 
-
+		/// @brief Gets the seed of the Random Generator.
+		/// @return The seed of the Random Generator
 		std::string getSeed();
+
+		/// @brief Sets the seed of this RandGen instance
+		/// @param seed The new seed of the RandGen
 		void setSeed(std::string* seed);
 
+		/// @brief Creates a uniform distribution between [min, max)
+		///		Throws if the provided min is not less than the max.
+		/// @param min The minimum number that can be returned.
+		/// @param max The maximum number that can be returned.
+		/// @throws std::runtime_error if min is greater than or equal to max
+		/// @return A random number between min and max
 		double uniDist(double min, double max);
 
 	protected:
--- GameBackbone/Source/Util/RandGen.cpp
+++ GameBackbone/Source/Util/RandGen.cpp
@@ -4,11 +4,6 @@
 
 using namespace GB;
 
-
-
-/// <summary>
-/// Initializes a new instance of the <see cref="RandGen"/> class with a random seed.
-/// </summary>
 RandGen::RandGen() {
 	m_seedString = new std::string;
 	m_generator = new std::mt19937;
@@ -22,11 +17,6 @@ RandGen::RandGen() {
 	delete seedString;
 }
 
-
-/// <summary>
-/// Initializes a new instance of the <see cref="RandGen"/> class.
-/// </summary>
-/// <param name="seed">The seed for the Generator.</param>
 RandGen::RandGen(std::string* seed) {
 	m_seedString = new std::string;
 	m_generator = new std::mt19937;
@@ -41,19 +31,10 @@ RandGen::~RandGen() {
 	delete m_uniDistributor;
 }
 
-/// <summary>
-/// Gets the seed of the Random Generator.
-/// </summary>
-/// <returns></returns>
 std::string RandGen::getSeed() {
 	return *m_seedString;
 }
 
-
-/// <summary>
-/// Sets the seed of the Random Generator.
-/// </summary>
-/// <param name="seed">New seed that we are setting on the engine.</param>
 void RandGen::setSeed(std::string* seed) {
 	// Save the string
 	*m_seedString = *seed;
@@ -63,13 +44,6 @@ void RandGen::setSeed(std::string* seed) {
 	m_generator->seed(tempSeed);
 }
 
-/// <summary>
-/// Creates a uniform distribution between [min, max)
-/// Throws if the provided min is not less than the max.
-/// </summary>
-/// <param name="min">The minimum number that can be returned.</param>
-/// <param name="max">The maximum number that can be returned.</param>
-/// <returns></returns>
 double RandGen::uniDist(double min, double max) {
 	if (min >= max)
 	{
