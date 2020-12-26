d62b123ee204e178eb74a64d49658421747b6c39
Ryan Lavin <lavinrp@gmail.com>
2020-12-26 18:19:22 -0500

    now correctly set VCPKG SHA
    now set the VCPKG sha by running VCPKG with a bad sha and letting it report what the correct SHA should be.

--- scripts/UpdateVcpkg.py
+++ scripts/UpdateVcpkg.py
@@ -4,6 +4,7 @@ import getopt
 import re
 import os
 import argparse
+import subprocess
 
 def set_agvcpkg_portfile_version_number(portfile_text, new_version_string):
     """
@@ -64,13 +65,28 @@ def update_vcpkg(gb_dir, vcpkg_gb_port_dir, sha):
     update_vcpkg_control(vcpkg_gb_port_dir, version_string)
     update_vcpkg_port(vcpkg_gb_port_dir, sha, version_string)
 
+def extract_sha_from_vcpkg_output(vcpkg_output: str):
+    sha_match = re.search(pattern=r"(Actual hash: \[ )([a-zA-Z0-9]+)( \])", string=vcpkg_output)
+    return sha_match.group(2)
+
+def run_vcpkg_with_wrong_sha(vcpkg_folder_path: str):
+    """
+    Run VCPKG with wrong SHA to get it to give us the correct SHA
+    """
+    result = subprocess.run([os.path.join(vcpkg_folder_path, "vcpkg.exe"), "install", "gamebackbone:x64-windows"], stdout=subprocess.PIPE)
+    sha = extract_sha_from_vcpkg_output(result.stdout.decode('utf-8'))
+    return sha
+
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Updates GB VCPKG port to new version")
-    parser.add_argument("--sha", type=str, help="git SHA of GameBackbone's new master branch", dest="sha")
     parser.add_argument("--gb_path", type=str, help="path to the top level folder of the GB repo", dest="gb_path")
     parser.add_argument("--gb_port_path", type=str, help="path to the GB port in VCPKG", dest="gb_port_path")
-
+    parser.add_argument("--vcpkg_folder_path", type=str, help="VCPKG folder", dest="vcpkg_folder_path")
     args = parser.parse_args()
-    update_vcpkg(args.gb_path, args.gb_port_path, args.sha)
-    
+
+    gb_port_path = os.path.join(args.vcpkg_folder_path, "ports/gamebackbone")
+    update_vcpkg(args.gb_path, gb_port_path, "1")
+    sha = run_vcpkg_with_wrong_sha(args.vcpkg_folder_path)
+    update_vcpkg(args.gb_path, gb_port_path, sha)
